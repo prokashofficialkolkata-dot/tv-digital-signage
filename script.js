ad0af0afd797f5e63ac0d5b6edbab2c458025fed
prokashofficialkolkata-dot <prokashofficial.kolkata@gmail.com>
2026-07-31 10:37:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,233 +1,71 @@
-let db;
+// GitHub Upload Settings
 
-let current=0;
-
-let slides=[];
-
-
-
-let request=indexedDB.open(
-"TVSlideDB",
-1
-);
+const GITHUB_USERNAME = "prokashofficialkolkata-dot";
+const REPOSITORY_NAME = "tv-digital-signage";
+const IMAGE_FOLDER = "images";
 
 
+// Upload Images Function
 
-request.onsuccess=function(e){
+async function uploadImages(){
 
-db=e.target.result;
+    const files = document.getElementById("images").files;
+    const message = document.getElementById("message");
 
-loadImages();
-
-};
-
-
-
-
-
-function loadImages(){
+    if(files.length === 0){
+        message.innerHTML = "Please select images first";
+        return;
+    }
 
 
-let transaction=db.transaction(
-["images"],
-"readonly"
-);
+    message.innerHTML = "Uploading images...";
 
 
-let store=transaction.objectStore("images");
+    for(let file of files){
+
+        const reader = new FileReader();
 
 
-let list=[];
+        reader.onload = async function(){
+
+            const base64Image = reader.result.split(",")[1];
 
 
-
-store.openCursor().onsuccess=function(e){
-
-
-let cursor=e.target.result;
+            const fileName = Date.now() + "-" + file.name;
 
 
-
-if(cursor){
-
-
-list.push(cursor.value.image);
+            const url =
+            `https://api.github.com/repos/${GITHUB_USERNAME}/${REPOSITORY_NAME}/contents/${IMAGE_FOLDER}/${fileName}`;
 
 
-cursor.continue();
+            await fetch(url,{
+                method:"PUT",
+
+                headers:{
+                    "Authorization":"Bearer YOUR_TOKEN_HERE",
+                    "Content-Type":"application/json"
+                },
 
 
-}
+                body:JSON.stringify({
 
-else{
+                    message:"Upload slide image",
 
+                    content:base64Image
 
-createSlides(list);
+                })
 
-
-}
-
-
-
-};
+            });
 
 
+        };
+
+
+        reader.readAsDataURL(file);
+
+    }
+
+
+    message.innerHTML="All images uploaded successfully";
 
 }
-
-
-
-
-
-function createSlides(images){
-
-
-let box=document.getElementById(
-"slideshow"
-);
-
-
-box.innerHTML="";
-
-
-
-images.forEach(function(img,index){
-
-
-let image=document.createElement("img");
-
-
-image.src=img;
-
-
-image.className="slide";
-
-
-
-if(index===0){
-
-image.classList.add("active");
-
-}
-
-
-
-box.appendChild(image);
-
-
-
-});
-
-
-
-slides=document.querySelectorAll(".slide");
-
-
-
-startSlide();
-
-
-}
-
-
-
-
-
-
-function startSlide(){
-
-
-
-if(slides.length<=1){
-
-return;
-
-}
-
-
-
-setInterval(function(){
-
-
-
-slides[current].classList.remove(
-"active"
-);
-
-
-
-current++;
-
-
-
-if(current>=slides.length){
-
-current=0;
-
-}
-
-
-
-slides[current].classList.add(
-"active"
-);
-
-
-
-},15000);
-
-
-
-}
-
-
-
-
-
-
-
-function updateClock(){
-
-
-let now=new Date();
-
-
-
-document.getElementById("time").innerHTML =
-
-now.toLocaleTimeString("en-MY",{
-
-timeZone:"Asia/Kuala_Lumpur",
-
-hour12:false
-
-});
-
-
-
-document.getElementById("date").innerHTML =
-
-now.toLocaleDateString("en-MY",{
-
-timeZone:"Asia/Kuala_Lumpur",
-
-weekday:"long",
-
-day:"2-digit",
-
-month:"long",
-
-year:"numeric"
-
-});
-
-
-
-}
-
-
-
-updateClock();
-
-
-setInterval(updateClock,1000);
